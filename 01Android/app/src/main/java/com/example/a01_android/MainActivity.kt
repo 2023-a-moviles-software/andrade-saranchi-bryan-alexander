a8d4f04319c29d317bead38a31edcb959de0e21d
Bryan Andrade <alexanderandrade252@gmail.com>
2023-06-22 11:01:50 -0500

    Envio de datos

--- 01Android/app/src/main/java/com/example/a01_android/MainActivity.kt
+++ 01Android/app/src/main/java/com/example/a01_android/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.a01_android
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    val callBackContenidoImplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if(result.resultCode == Activity.RESULT_OK){
+                if(result.data != null){
+                    //Logica de negocio
+                    val data = result.data
+                    "${data?.getStringExtra("nombreModificado")}"
+                }
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,7 +49,23 @@ class MainActivity : AppCompatActivity() {
         clase: Class <*>
     ){
         val intent = Intent(this, clase)
+        //NO RECIBIMOS RESPUESTA
         startActivity(intent)
+    }
+
+
+    fun abrirActividadConParametros(
+        clase: Class<*>
+    ){
+        val intentExplicito = Intent(this,clase)
+        //Enviar parametros
+        //(aceptamos primitivas)
+        intentExplicito.putExtra("nombre", "Adrian")
+        intentExplicito.putExtra("apellido", "Eguez")
+        intentExplicito.putExtra("edad", 30)
+        //enviamos intent con RESPUESTA
+        callBackContenidoImplicito
+            .launch(intentExplicito)
     }
 }
 
